f9a03db13e7e2ea86eb6dae45f5db2a98fcaddff
ERIC FIGUEIREDO <djericmax@hotmail.com>
2020-10-19 03:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Account/LoginInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreAuth.Pages.Account
+{
+    public class LoginInputModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Pages/Account/login.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreAuth.Pages.Account
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost(LoginInputModel inputModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (!IsUserAuthenticated(inputModel.UserName, inputModel.Password))
+            {
+                ModelState.AddModelError(string.Empty, "Usuário ou senha inválidos");
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, inputModel.UserName)
+            };
+
+            var userIdentity = new ClaimsIdentity(claims, "login");
+            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+            await HttpContext.SignInAsync(principal);
+
+            return Redirect("/");
+        }
+
+        public async Task<IActionResult> OnPostLogout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/Account/Login");
+        }
+
+        private bool IsUserAuthenticated(string userName, string password)
+        {
+            //simulação de autenticação no banco de daos.
+            return true;
+        }
+    }
+}
